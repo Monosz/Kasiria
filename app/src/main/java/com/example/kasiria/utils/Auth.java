e22db615aebb17752bf06f2bb253664403b9024f
Monos <monmadnesss@gmail.com>
2025-01-14 23:04:49 +0700

    Fix auth util getUid logic

--- app/src/main/java/com/example/kasiria/utils/Auth.java
+++ app/src/main/java/com/example/kasiria/utils/Auth.java
@@ -2,6 +2,8 @@ package com.example.kasiria.utils;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.Objects;
+
 public class Auth {
     private static final FirebaseAuth auth = FirebaseAuth.getInstance();
 
@@ -11,7 +13,7 @@ public class Auth {
 
     public static String getUid() {
         if (isLoggedIn()) {
-            return auth.getUid();
+            return Objects.requireNonNull(auth.getCurrentUser()).getUid();
         }
         return "";
     }
